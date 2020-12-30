f5f84c829fa0cbf4f4d93828060490fd7fbbf048
Matthew Daly <Matthew Daly 450801+matthewbdaly@users.noreply.github.com>
2020-12-30 14:15:43 +0000

    Now returns PNG files

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,22 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home', [
+        'example' => route('placeholder', ['width' => 50, 'height' => 50]),
+    ]);
 });
+Route::get('/placeholder/{width}x{height}', function (int $width, int $height) {
+    if (!$img = imagecreatetruecolor($width, $height)) {
+        abort();
+    }
+    $textColour = imagecolorallocate($img, 255, 255, 255);
+    imagestring($img, 1, 5, 5, "$width X $height", $textColour);
+    ob_start();
+    imagepng($img);
+    $file = ob_get_contents();
+    ob_end_clean();
+    return response()->make($file, 200, [
+        'Content-type' => 'image/png'
+    ]);
+})->where(['width' => '[0-9]+', 'height' => '[0-9]+'])
+    ->name('placeholder');
